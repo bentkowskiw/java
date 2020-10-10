a86c7f6683be7dd94bdb29cce183c092c9347a83
Wojciech <bentkowski@codogrosza.pl>
2020-10-10 16:18:13 +0200

    notation transformer class

---
+++ src/main/java/bentkowski/io/bentkowski/NotationTransformer.java
@@ -0,0 +1,16 @@
+package bentkowski.io.bentkowski;
+
+import java.math.BigInteger;
+
+public class NotationTransformer {
+
+    private int number;
+
+    public NotationTransformer(int number) {
+        this.number = number;
+    }
+
+    public char[][] transformNumber()    {
+        return null;
+    }
+}
